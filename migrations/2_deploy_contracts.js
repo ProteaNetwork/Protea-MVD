691e96a3476f9059be176726c4d2e10a03b06fff
Unknown <ryanjnoble@gmail.com>
2018-04-11 11:45:39 +0200

    Intergrating to FE

--- migrations/2_deploy_contracts.js
+++ migrations/2_deploy_contracts.js
@@ -1,21 +1,14 @@
-const ProteaToken = artifacts.require("ProteaToken");
-const CommunityManager = artifacts.require("CommunityManager");
+const ETHConference = artifacts.require("ETHConference");
+const CompliantToken = artifacts.require("CompliantToken");
 
 const fs = require('fs');
 
-module.exports = function(deployer, network) {
+module.exports = function (deployer, network) {
     const deployConfig = JSON.parse(fs.readFileSync('./config/deploy.json'));
-
-    async function setPost() {
-        const protea = await ProteaToken.deployed();
-        const CommunityManager = await CommunityManager.deployed();
-    }
 
     deployer.then(async () => {
         await deployer.deploy(ProteaToken, deployConfig.protea.supply, deployConfig.protea.name, deployConfig.protea.decimals,
             deployConfig.protea.symbol);
         await deployer.deploy(CommunityManager, ProteaToken.address);
-        
-        await setPost();
     })
 };
